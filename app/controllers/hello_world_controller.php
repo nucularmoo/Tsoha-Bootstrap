c102a823ab2034ee3d5e2ba1cdc251a910d64439
NukeCow <erika.jarvinen@helsinki.fi>
2016-08-08 18:47:41 +0300

    HTML-pohjat

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -2,13 +2,25 @@
 
   class HelloWorldController extends BaseController{
 
-    public static function index(){
-      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-	echo 'Tämä on etusivu';
-    }
+	public static function mon_list(){
+		View:: make('suunnitelmat/mon_list.html');
+	}	
 
-    public static function sandbox(){
+	public static function login(){
+		View:: make('suunnitelmat/login.html');
+	}
+
+	public static function mon_show(){
+		View::make('suunnitelmat/mon_show.html');
+	}
+
+    	public static function index(){
+      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
+		echo 'Tämä on etusivu';
+   	 }
+
+   	 public static function sandbox(){
       // Testaa koodiasi täällä
-      View::make('helloworld.html');
-    }
+     		 View::make('helloworld.html');
+    	}
   }
--- config/routes.php
+++ config/routes.php
@@ -1,5 +1,17 @@
 <?php
 
+  $routes->get('/mons', function() {
+	HelloWorldController::mon_list();
+});
+
+  $routes->get('/login', function() {
+	HelloWorldController::login();
+});
+
+  $routes->get('/mons/1', function() {
+	HelloWorldController::mon_show();
+});
+
   $routes->get('/', function() {
     HelloWorldController::index();
   });
