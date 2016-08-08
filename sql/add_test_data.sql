e8120265d6155ff4eed2d27904075e4a998c0626
NukeCow <erika.jarvinen@helsinki.fi>
2016-08-08 17:40:55 +0300

    sql-jutut

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,27 @@
--- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO trainer (name, password) VALUES ('Besttrainereu', 'besttrainereu123');
+INSERT INTO trainer (name, password) VALUES ('Besttrainerna', 'besttrainerna123');
+
+INSERT INTO typedesc (name) VALUES('Normal');
+INSERT INTO typedesc (name) VALUES('Fighting');
+INSERT INTO typedesc (name) VALUES('Flying');
+INSERT INTO typedesc (name) VALUES('Poison');
+INSERT INTO typedesc (name) VALUES('Ground');
+INSERT INTO typedesc (name) VALUES('Rock');
+INSERT INTO typedesc (name) VALUES('Bug');
+INSERT INTO typedesc (name) VALUES('Ghost');
+INSERT INTO typedesc (name) VALUES('Ghost');
+INSERT INTO typedesc (name) VALUES('Steel');
+INSERT INTO typedesc (name) VALUES('Fire');
+INSERT INTO typedesc (name) VALUES('Water');
+INSERT INTO typedesc (name) VALUES('Grass');
+INSERT INTO typedesc (name) VALUES('Electric');
+INSERT INTO typedesc (name) VALUES('Psychic');
+INSERT INTO typedesc (name) VALUES('Ice');
+INSERT INTO typedesc (name) VALUES('Dragon');
+INSERT INTO typedesc (name) VALUES('Dark');
+INSERT INTO typedesc (name) VALUES('Fairy');
+
+INSERT INTO attack (name, damage, dps) VALUES ('Wing attack', '9', '12');
+
+INSERT INTO pokemon (dexnumber, name) VALUES ('42', 'Zubat');
+INSERT INTO pokemon (dexnumber, name, attack, defense, stamina, jauheliha) VALUES ('25', 'Pikachu', '124', 108, 70, 50);
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,47 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE trainer(
+	id SERIAL PRIMARY KEY,
+	name varchar(50) NOT NULL,
+	password varchar(50) NOT NULL
+);
+
+CREATE TABLE typedesc(
+	id SERIAL PRIMARY KEY,
+	name varchar(50) NOT NULL
+);
+
+CREATE TABLE attack(
+	id SERIAL PRIMARY KEY,
+	name varchar(50) NOT NULL,
+	damage INT NOT NULL,
+	dps INT NOT NULL,
+	typedesc_id INTEGER REFERENCES typedesc(id)
+); 
+
+CREATE TABLE pokemon(
+	id SERIAL PRIMARY KEY,
+	dexnumber INT NOT NULL,
+	name varchar(50) NOT NULL,
+	typedesc_a_id INTEGER REFERENCES typedesc(id),
+	typedesc_b_id INTEGER REFERENCES typedesc(id),
+	attack INT,
+	defense INT,
+	stamina INT,
+	evolution_id INTEGER REFERENCES pokemon(id),
+	jauheliha INT
+);
+
+CREATE TABLE pokemonmoves(
+	pokemon_id INTEGER REFERENCES pokemon(id),
+	attack_id INTEGER REFERENCES attack(id)
+);
+
+CREATE TABLE pokedex(
+	trainer_id INTEGER REFERENCES trainer(id),
+	pokemon_id INTEGER REFERENCES pokemon(id)
+);
+
+CREATE TABLE super_effective(
+	strong_type_id INTEGER REFERENCES typedesc(id),
+	weak_type_id INTEGER REFERENCES typedesc(id)
+);
+	
